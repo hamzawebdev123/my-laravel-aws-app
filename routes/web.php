211092a17b782f77dfda0277c5e4ee4a8ab6876b
hamzawebdev123 <githubbyhamza@example.com>
2026-02-10 19:24:28 +0500

    Finalized deploy script with auto migrations

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\InvoiceController;
 Route::get('/hello', function () {
     return view('home');
 });
-Route::get('/', [InvoiceController::class, 'create']);
+Route::get('/invoices', [InvoiceController::class, 'create']);
 Route::post('/invoice/download', [InvoiceController::class, 'download']);
-Route::get('/invoices', [InvoiceController::class, 'index']);
+Route::get('/', [InvoiceController::class, 'index']);
 Route::get('/invoice/download/{id}', [InvoiceController::class, 'reDownload'])->name('invoice.redownload');
